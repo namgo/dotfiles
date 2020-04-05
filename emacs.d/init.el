68a0c11740d4d58bc846e1531c401bb00066943c
Namgo <nahumdeltoro@riseup.net>
2020-04-05 12:56:21 -0400

    Better emacs config also browser selection

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,14 +1,24 @@
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")))
 
-
-
-
 (package-initialize)
-
 
 ;; load custom file 
 (setq custom-file "~/.emacs.d/custom.el")
 
 (unless (file-exists-p custom-file)
   (with-temp-buffer (write-file custom-file)))
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(use-package solarized-theme
+  :config (load-theme 'solarized-dark t)
+  :ensure t)
+
+(set-face-attribute 'default nil
+                    :family "Hack"
+                    :height 110
+                    :weight 'normal
+                    :width 'normal)
